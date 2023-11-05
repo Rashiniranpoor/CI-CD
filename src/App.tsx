450594f7680726796e6f803e15704ab9ffefb908
Rashin <131351840+Rashiniranpoor@users.noreply.github.com>
2023-11-05 17:25:18 +0100

    pass test

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() { 
 
   return (
-    <>
+    <div>
       <h1>Hello world!</h1>
       <h2>This is test</h2>
-    </>
+    </div>
   )
 }
 
--- src/test/app.test.tsx
+++ src/test/app.test.tsx
@@ -4,7 +4,7 @@ import App from "../App";
 describe("App", () => {
   it("renders headline", () => {
     render(<App />);
-    const title = screen.getByText(/Rashin/i);
+    const title = screen.getByText(/world/i);
     expect(title).toBeInTheDocument();
   });
 });
